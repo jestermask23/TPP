27f7ffaeab8f0b8b42906c78b5e40ab25ed618f4
jestermask23 <nizar.skorik@gmail.com>
2026-02-06 00:22:10 +0300

    HW5 added

--- Homeworks/HW4/Program.cs
+++ Homeworks/HW4/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Group { get; set; }
+
+    public Student(string name, int age, string group)
+    {
+        Name = name;
+        Age = age;
+        Group = group;
+    }
+
+    public void Study()
+    {
+        Console.WriteLine($"Студент по имени {Name}, которому {Age} лет, учится в группе {Group}.");
+    }
+}
+
+
+class Master : Student
+{
+    public Master(string name, int age, string group) : base(name, age, group) { }
+
+    public void DefendThesis()
+    {
+        Console.WriteLine($"{Name} готовится к защите диплома и проводит научное исследование.");
+    }
+}
+
+
+class Bachelor : Student
+{
+    public Bachelor(string name, int age, string group) : base(name, age, group) { }
+
+    public void PassExams()
+    {
+        Console.WriteLine($"{Name} сдаёт итоговые экзамены по выбранным дисциплинам.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student = new Student("Иван", 19, "ИТ-101");
+        student.Study();
+
+        Master master = new Master("Анна", 24, "МТ-202");
+        master.Study();
+        master.DefendThesis();
+
+        Bachelor bachelor = new Bachelor("Пётр", 21, "БК-303");
+        bachelor.Study();
+        bachelor.PassExams();
+    }
+}
---
+++ Homeworks/HW5/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+// Интерфейс получения урона
+interface IDamageable
+{
+    void TakeDamage(int damage);
+}
+
+// Абстрактный класс персонажа
+abstract class Character : IDamageable
+{
+    private string name;
+    private int health;
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public int Health
+    {
+        get { return health; }
+    }
+
+    protected Character(string name, int health)
+    {
+        this.name = name;
+        this.health = health;
+    }
+
+    public abstract void Attack();
+
+    public void Move()
+    {
+        Console.WriteLine($"{name} перемещается.");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Console.WriteLine($"{name} получил {damage} урона. Осталось здоровья: {health}");
+    }
+}
+
+// Воин
+class Warrior : Character
+{
+    public Warrior(string name, int health)
+        : base(name, health)
+    {
+    }
+
+    public override void Attack()
+    {
+        Console.WriteLine($"{Name} наносит удар мечом!");
+    }
+}
+
+// Маг
+class Mage : Character
+{
+    public Mage(string name, int health)
+        : base(name, health)
+    {
+    }
+
+    public override void Attack()
+    {
+        Console.WriteLine($"{Name} бросает огненный шар!");
+    }
+}
+
+// Лучник
+class Archer : Character
+{
+    public Archer(string name, int health)
+        : base(name, health)
+    {
+    }
+
+    public override void Attack()
+    {
+        Console.WriteLine($"{Name} выпускает стрелу из лука!");
+    }
+}
+
+// Точка входа
+class Program
+{
+    static void Main()
+    {
+        Character[] characters = new Character[]
+        {
+            new Warrior("Артур", 100),
+            new Mage("Вова", 80),
+            new Archer("Робин", 90)
+        };
+
+        foreach (Character character in characters)
+        {
+            character.Attack();
+            character.Move();
+            character.TakeDamage(10);
+            Console.WriteLine();
+        }
+    }
+}
